17dd5d23abd3be3eba2e788c0a252ff6869df8b1
ozannicholls <116882906+ozannicholls@users.noreply.github.com>
2022-11-11 13:03:39 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,36 @@ function setup() {
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if (mouseX < width/2){
+        //text('LEFT', 10, 100)
+        console.log('LEFT')
+
+        fill('#FFE600')
+        circle(width/2, height/2, 200)//head
+
+        circle(width/2 - 35, width/2 - 35, 30)//left eye
+        circle(width/2 + 35, width/2 - 35, 30)//right eye
+
+        arc(width/2, height/2 + 30, 80, 80, 0, PI + TWO_PI, OPEN);//smile
+    }
+    if (mouseX > width/2){
+        //text('RIGHT', width/2+10, 100)
+        console.log('RIGHT')
+
+        fill('#690000')
+        circle(width/2, height/2, 200)//head
+
+        circle(width/2 - 35, width/2 - 35, 30)//left eye
+        circle(width/2 + 35, width/2 - 35, 30)//right eye
+
+        arc(width/2, height/2 + 60, 80, 80, PI + TWO_PI, 0, OPEN);//frown
+    }
+    //fill('#FFE600')
+    //circle(width/2, height/2, 200)//head
+
+    //circle(width/2 - 35, width/2 - 35, 30)//left eye
+    //circle(width/2 + 35, width/2 - 35, 30)//right eye
+
+    //arc(width/2, height/2 + 30, 80, 80, 0, PI + TWO_PI, OPEN);//smile
+    //arc(width/2, height/2 + 60, 80, 80, PI + TWO_PI, 0, OPEN);//frown
 }
